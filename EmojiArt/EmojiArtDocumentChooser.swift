709d16c331c9d4f233eb3dc154958ed5397474d0
pascalhostettler <pascal.hostettler@students.fhnw.ch>
2021-01-08 20:44:18 +0100

    implements grid view with title

--- EmojiArt/EmojiArtDocumentChooser.swift
+++ EmojiArt/EmojiArtDocumentChooser.swift
@@ -11,42 +11,85 @@ struct EmojiArtDocumentChooser: View {
     @EnvironmentObject var store: EmojiArtDocumentStore
     
     @State private var editMode: EditMode = .inactive
+    @State private var showGridView = false
+
     
     var body: some View {
+    
+        if showGridView == true {
+            
         NavigationView {
-            List {
-                ForEach(store.documents) { document in
+                Grid(store.documents) { document in
                     NavigationLink(destination: EmojiArtDocumentView(document: document)
                                     .navigationBarTitle(self.store.name(for:document))
                     ) {
+                        //VIEW ANZEIGEN
                         EditableText(self.store.name(for: document), isEditing: self.editMode.isEditing) { name in
                             self.store.setName(name, for: document)
                         }
 
                     }
                 }
-                .onDelete { indexSet in
-                    indexSet.map { self.store.documents[$0] }.forEach { document in
-                        self.store.removeDocument(document)
+                .navigationBarTitle(self.store.name)
+                .navigationBarItems(leading: Button(action: {
+                    self.store.addDocument()
+                }, label: {
+                    Image(systemName: "plus").imageScale(.large)
+                }),
+                trailing:
+                    HStack {
+                    
+                        Button(action: {
+                            self.showGridView = false
+                        }, label: {
+                            Image(systemName: "list.dash").imageScale(.large)
+                        })
+                        EditButton()
+                    }
+                )
+                .environment(\.editMode, $editMode)
+        }.animation(.default)
+            } else {
+                NavigationView {
+                List {
+                    ForEach(store.documents) { document in
+                        NavigationLink(destination: EmojiArtDocumentView(document: document)
+                                        .navigationBarTitle(self.store.name(for:document))
+                        ) {
+                            EditableText(self.store.name(for: document), isEditing: self.editMode.isEditing) { name in
+                                self.store.setName(name, for: document)
+                            }
+
+                        }
+                    }
+                    .onDelete { indexSet in
+                        indexSet.map { self.store.documents[$0] }.forEach { document in
+                            self.store.removeDocument(document)
+                        }
                     }
                 }
-            }
+            
             .navigationBarTitle(self.store.name)
             .navigationBarItems(leading: Button(action: {
                 self.store.addDocument()
             }, label: {
                 Image(systemName: "plus").imageScale(.large)
             }),
-            trailing: EditButton()
+            trailing:
+                HStack {
+                    Button(action: {
+                        self.showGridView = true
+                    }, label: {
+                        Image(systemName: "rectangle.split.3x3").imageScale(.large)
+                    })
+                    EditButton()
+                }
             )
             .environment(\.editMode, $editMode)
         }
+                .animation(.default)
 
     }
+
 }
-
-struct EmojiArtDocumentChooser_Previews: PreviewProvider {
-    static var previews: some View {
-        EmojiArtDocumentChooser()
-    }
 }
